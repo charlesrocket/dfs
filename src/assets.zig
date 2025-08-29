e9ed65b8f6db8030e46b0cf52001e4a3cd5e4691
-k <slowdive@me.com>
2025-08-29 11:34:14 -0400

    feat(cli): add assets

---
+++ src/assets.zig
@@ -0,0 +1,22 @@
+pub const help_message =
+    \\____________ _____
+    \\|  _  \  ___/  ___|
+    \\| | | | |_  \ `--.
+    \\| | | |  _|  `--. \
+    \\| |/ /| |   /\__/ /
+    \\|___/ \_|   \____/
+    \\
+    \\{s}{s}
+    \\{s}{s}
+    \\
+    \\
+;
+
+pub const help_prefix =
+    \\┌───┐
+    \\│dfs│
+    \\└───┘
+;
+
+pub const opt_usage = "{u}{?u}{s}{s}{s?}{s} <{s}>";
+pub const examples_header = "{s}EXAMPLE:\n";
