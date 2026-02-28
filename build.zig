9ed431a5c1abfdd026283a480bfc72d6c546a7eb
-k <slowdive@me.com>
2026-02-28 08:38:36 -0500

    build: fix library name

--- build.zig
+++ build.zig
@@ -11,7 +11,7 @@ pub fn build(b: *std.Build) void {
 
     const lib = b.addLibrary(.{
         .linkage = .static,
-        .name = "DFS library",
+        .name = "libdfs",
         .root_module = lib_mod,
     });
 
